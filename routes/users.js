3d86368e4014c74ef041b0323672ded0ed8aca87
Leo Lee <tl.leolee@gmail.com>
2019-11-17 02:57:04 +0000

    added cookie session middleware in server, also added login and logout route in users route accepting post request on login and logout

--- routes/users.js
+++ routes/users.js
@@ -21,40 +21,67 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      if (email) {
+        const dbRes = await db.query(`SELECT * FROM users WHERE email = '${email}';`)
+        const user = dbRes.rows[0];
+        if (user && user.password == password) {
+          req.session.userId = user.id;
+          res.status(200).send({ user: { name: user.name, id: user.id, email: user.email } });
+        } else {
+          throw new Error ('Authentication failed');
+        }
+      } else {
+        throw new Error ('Input field is empty')
+      }
+    } catch (e) {
+      res.status(400).send({ user: {}, message: `login failed! ${e}`})
+    }
+    
+  })
+
+  router.post('/logout', (req, res) => {
+    req.session.userId = null;
+    res.sendStatus(200);
+  })
+
   return router;
 };
 
-const login =  function(email, password) {
-  return database.getUserWithEmail(email)
-  .then(user => {
-    if (bcrypt.compareSync(password, user.password)) {
-      return user;
-    }
-    return null;
-  });
-}
-exports.login = login;
+// const login =  function(email, password) {
+//   return database.getUserWithEmail(email)
+//   .then(user => {
+//     if (bcrypt.compareSync(password, user.password)) {
+//       return user;
+//     }
+//     return null;
+//   });
+// }
+// exports.login = login;
 
 
-router.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  login(email, password)
-    .then(user => {
-      if (!user) {
-        res.send({error: "error"});
-        return;
-      }
-      req.session.userId = user.id;
-      res.send({user: {name: user.name, email: user.email, id: user.id}});
-    })
-    .catch(e => res.send(e));
-});
+// router.post('/login', (req, res) => {
+//   const {email, password} = req.body;
+//   login(email, password)
+//     .then(user => {
+//       if (!user) {
+//         res.send({error: "error"});
+//         return;
+//       }
+//       req.session.userId = user.id;
+//       res.send({user: {name: user.name, email: user.email, id: user.id}});
+//     })
+//     .catch(e => res.send(e));
+// });
 
 
-router.post('/logout', (req, res) => {
-  req.session.userId = null;
-  res.send({});
-});
+// router.post('/logout', (req, res) => {
+//   req.session.userId = null;
+//   res.send({});
+// });
 
 // create this function for this route to work
 
@@ -63,22 +90,22 @@ router.post('/logout', (req, res) => {
 // redirects the object to the client which the header template uses
 // with getmydetails to hide the login button and show relevant user name
 
-router.get("/me", (req, res) => {
-  const userId = req.session.userId;
-  if (!userId) {
-    res.send({message: "not logged in"});
-    return;
-  }
+// router.get("/me", (req, res) => {
+//   const userId = req.session.userId;
+//   if (!userId) {
+//     res.send({message: "not logged in"});
+//     return;
+//   }
 
-database.getUserWithId(userId)
-.then(user => {
-  if (!user) {
-    res.send({error: "no user with that id"});
-    return;
-  }
+// database.getUserWithId(userId)
+// .then(user => {
+//   if (!user) {
+//     res.send({error: "no user with that id"});
+//     return;
+//   }
 
-  res.send({user: {name: user.name, email: user.email, id: userId}});
-})
-.catch(e => res.send(e));
-});
+//   res.send({user: {name: user.name, email: user.email, id: userId}});
+// })
+// .catch(e => res.send(e));
+// });
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT       = process.env.PORT || 8080;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
@@ -24,12 +25,19 @@ app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
   debug: true,
   outputStyle: 'expanded'
 }));
+
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
