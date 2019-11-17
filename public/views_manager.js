c98bb714bb8f24a9ad1ceeabf7e593f20994ccd0
Daniel Braverman <hexular@gmail.com>
2019-11-17 23:55:16 +0000

    add descriptions to views_manager page with logic to be implemented

--- public/views_manager.js
+++ public/views_manager.js
@@ -5,25 +5,60 @@ $(() => {
   window.views_manager = {};
 
   window.views_manager.show = function(item, data) {
+
+    // need to add logic to checkout footer to check if there are items already ***MVP***
+    // if so, do not detach, but route it to the update footer case ***STRETCH***
     $checkout_footer.detach();
+
+    // these three detach upon any navigation from their respective pages
     $checkout_item.detach();
     $checkout_page.detach();
     $confirmation.detach();
+
+    // DO NOT detach if item = add_item
     $food_options.detach();
+
+    // ONLY detach if deny, accept or order completed has been pressed
+    // need to make sure other routes don't force detach this
     $res_order_card.detach();
+
+    // this one should only detach if the restaurant logs out
     $res_order_viewer.detach();
-    $restaurant_carousel.detach();
+
+    // this should only detach when navigating to cart
+    // $restaurant_carousel.detach(); ***STRETCH***
+
+    // this should only detach on checkout
+    // or ***STRETCH*** when picking a new restaurant and rerendering the name
     $restaurant_listing.detach();
 
     switch (item) {
-      case 'food_options':
-        data.restaurant_id = $restaurant.restaurant_id;
-        $food_options.appendTo($main);
+      // both cart and checkout footer should trigger this
+      case 'checkout':
+        $checkout_page.appendTo($main);
         break;
+      case 'place_order':
+        $confirmation.appenTo($main);
+        break;
+      case 'add_item':
+        // only show the footer if there were no items previously
+        // control the logic in the source for 'add_item'
+        $checkout_footer.appendTo($main);
+        break;
+      case 'update_footer':
+        // should send this to a route that updates the display of the checkout footer
+        // ***STRETCH***
+        break;
+      case 'accept_order':
+        // will need to specify which part exactly from within res_order_viewer
+        $res_order_card.appendTo($res_order_viewer);
+
+      // case 'food_options':
+      //   data.restaurant_id = $restaurant.restaurant_id;
+      //   $food_options.appendTo($main);
+      //   break;
       case '':
-        $newPropertyForm.appendTo($main);
-        break;
-      case 'reservations':
+
 
       case 'error': {
         const $error = $(`<p>${arguments[1]}</p>`);
