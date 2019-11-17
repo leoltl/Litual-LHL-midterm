817160a4b574416bdfb7545e4f61fe3728d1f2b1
Jay Chai <chunsaa@gmail.com>
2019-11-17 01:57:48 +0000

    added food_options case

--- public/views_manager.js
+++ public/views_manager.js
@@ -5,23 +5,25 @@ $(() => {
   window.views_manager = {};
 
   window.views_manager.show = function(item, data) {
-    $checkout_footer.detach();
+    /* $checkout_footer.detach();
     $checkout_item.detach();
     $checkout_page.detach();
     $confirmation.detach();
-    $food_options.detach();
+
     $res_order_card.detach();
     $res_order_viewer.detach();
     $restaurant_carousel.detach();
-    $restaurant_listing.detach();
+    $restaurant_listing.detach(); */
+    $logInForm.detach();
+    $food_options.detach();
 
     switch (item) {
       case 'food_options':
-        data.restaurant_id = $restaurant.restaurant_id;
+        //data.restaurant_id = $restaurant.restaurant_id;
         $food_options.appendTo($main);
         break;
-      case '':
-        $newPropertyForm.appendTo($main);
+      case 'logIn':
+        $logInForm.appendTo($main);
         break;
       case 'reservations':
 
