13725d645a8794d1de0d6dceb86c0c4039eb434d
Jay Chai <chunsaa@gmail.com>
2019-11-17 01:56:28 +0000

    removed and applied the code to food_options.js

--- public/scripts/update-cart-total.js
+++ public/scripts/update-cart-total.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-  $("footer").hide();
-  let totalItemsInCart = parseInt($("footer").text().split(":")[1].trim());
-  $(".add-to-cart.form-control").on('click', function(event) {
-    event.preventDefault();
-    const quantity = parseInt($(this).closest("form").find("option:selected").val());
-    totalItemsInCart += quantity;
-    $("footer").text(`items in cart: ${totalItemsInCart}`);
-    $("footer").show();
-  });
+
 });
