04164fe84954a304a675a47bc5f4cdd409ecab40
Jay Chai <chunsaa@gmail.com>
2019-11-18 16:01:23 +0000

    refactor code to put all header related code to own file

--- public/templates/checkout_footer.js
+++ public/templates/checkout_footer.js
@@ -2,3 +2,22 @@
   this will pop up once any one of the items has been added to cart
   this will redirect to the checkout page with a list of the items and quntities selected
 */
+$(() => {
+  window.footer = {};
+
+  const $pageFooter = $('.checkout-footer');
+  let currentUser = null;
+  function updateFooter(user) {
+    currentUser = user;
+    if (!user) {
+      footer = $(`<p>items in cart: 0</p>
+      <button class="cart">Go To Cart</button>`);
+    } else {
+      footer = "" //TOADD
+    }
+    $pageFooter.append(footer);
+  }
+
+  updateFooter(undefined);
+
+});
