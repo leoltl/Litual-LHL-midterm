4de627470cd458cb2811afb5ad668ca1b6b7bc47
Jay Chai <chunsaa@gmail.com>
2019-11-19 14:37:41 +0000

    add event handler for adding items to cart

--- public/templates/food_options.js
+++ public/templates/food_options.js
@@ -24,7 +24,7 @@ $(() => {
   }
 
   function updateMenu(item) {
-    console.log(item)
+    //console.log(item)
     const $food_options = $(`
 
         <!-- start food-options item component -->
@@ -37,14 +37,17 @@ $(() => {
             <h5 class="price-tag">$${item.price}/order</h5>
             <form action="/order" method="AJAXPOST--TODO">
               <input type="number" min="0">
-              <input class="add-to-cart call-to-action form-control" type="submit" value="Add To Cart">
+              <input data-itemid="${item.id}" id="item${item.id}" class="add-to-cart call-to-action form-control" type="submit" value="Add To Cart">
             </form>
           </div>
         </article>
         <!-- end food-options item component -->
 
     `);
-
+    localStorage.setItem(`item${item.id}Price`, item.price.toString());
+    localStorage.setItem(`item${item.id}Name`, item.name);
+    localStorage.setItem(`item${item.id}Quantity`, "0");
+    console.log(localStorage[`item${item.id}Price`]);
     window.$food_options = $food_options;
     $('main').append($food_options);
   }
@@ -57,43 +60,57 @@ $(() => {
       }
     });
 
+
+
   $(document).ready(function() {
-    let item1Quantity = 0;
-    let item2Quantity = 0;
-    let item3Quantity = 0;
+    //console.log("yo", localStorage[`item1Quantity`]);
+    /* let item1Quantity = localStorage[`item1Quantity`] ? parseInt(localStorage[`item1Quantity`]) : 0;
+    let item2Quantity = localStorage[`item2Quantity`] ? parseInt(localStorage[`item2Quantity`]) : 0;
+    let item3Quantity = localStorage[`item3Quantity`] ? parseInt(localStorage[`item3Quantity`]) : 0; */
+
+    /* localStorage.setItem("item1Quantity", "0");
+    localStorage.setItem("item2Quantity", "0");
+    localStorage.setItem("item3Quantity", "0");
+    console.log(localStorage["item4Quantity"]); */
 
     $("footer").hide();
-    let totalItemsInCart = parseInt($("footer").text().split(":")[1].trim());
 
-    $("#item1.form-control").on('click', function(event) {
-      console.log("item1");
-      const quantity = parseInt($(this).closest("form").find("option:selected").val());
-      item1Quantity += quantity;
-      totalItemsInCart += quantity;
-      console.log(item1Quantity);
+    //parseInt($("footer").text().split(":")[1].trim());
+
+  /*   $("body").on('click', "#item1", function(event) {
+      console.log("clicked item1");
+      localStorage["item1Quantity"] = (parseInt(localStorage[`item1Quantity`]) + 1).toString();
+      //totalItemsInCart++;
+      //console.log(item1Quantity);
     });
 
-    $("#item2.form-control").on('click', function(event) {
+    $("body").on('click', "#item2", function(event) {
       console.log("item2");
-      const quantity = parseInt($(this).closest("form").find("option:selected").val());
-      item2Quantity += quantity;
-      totalItemsInCart += quantity;
-      console.log(item2Quantity);
+      localStorage["item2Quantity"] = (parseInt(localStorage[`item2Quantity`]) + 1).toString();
+      //item2Quantity++;
+      //totalItemsInCart++;
+      //console.log(item2Quantity);
     });
 
-    $("#item3.form-control").on('click', function(event) {
+    $("body").on('click', "#item3", function(event) {
       console.log("item3");
-      const quantity = parseInt($(this).closest("form").find("option:selected").val());
-      item3Quantity += quantity;
-      totalItemsInCart += quantity;
-      console.log(item3Quantity);
-    });
+      localStorage["item3Quantity"] = (parseInt(localStorage[`item3Quantity`]) + 1).toString();
+      //item3Quantity++;
+      //totalItemsInCart++;
+      //console.log(item3Quantity);
+    }); */
 
 
-    $(".add-to-cart.form-control").on('click', function(event) {
+    $("body").on('click', ".add-to-cart.form-control", function(event) {
       event.preventDefault();
-      //const quantity = parseInt($(this).closest("form").find("option:selected").val());
-      //totalItemsInCart += item1Quantity;
+      console.log("hello");
+      console.log($(this).data("itemid"));
+      const id = $(this).data("itemid");
+      let totalItemsInCart = parseInt($("footer p").text().split(":")[1]);
+      console.log("yolo", totalItemsInCart);
+      localStorage[`item${id}Quantity`] = (parseInt(localStorage[`item${id}Quantity`]) + 1).toString();
+      //let totalItemsInCart = parseInt(localStorage["item1Quantity"]) + parseInt(localStorage["item2Quantity"]) + parseInt(localStorage["item3Quantity"]);
+      totalItemsInCart++;
       $("footer p").text(`items in cart: ${totalItemsInCart}`);
       $("footer").show();
     });
@@ -101,9 +118,9 @@ $(() => {
     $("footer .cart").on('click', function(event) {
       event.preventDefault();
       //console.log(localStorage);
-      localStorage.setItem("item1", item1Quantity.toString());
-      localStorage.setItem("item2", item2Quantity.toString());
-      localStorage.setItem("item3", item3Quantity.toString());
+      /* localStorage.setItem("item1Quantity", item1Quantity.toString());
+      localStorage.setItem("item2Quantity", item2Quantity.toString());
+      localStorage.setItem("item3Quantity", item3Quantity.toString()); */
 
       $("footer").hide();
       views_manager.show("checkout");
