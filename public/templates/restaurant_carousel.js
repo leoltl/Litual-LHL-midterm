0f365a1464aba20cf36c9291af46ce549b2d42fd
Leo Lee <tl.leolee@gmail.com>
2019-11-21 22:13:49 +0000

    added restaurant-card css

--- public/templates/restaurant_carousel.js
+++ public/templates/restaurant_carousel.js
@@ -4,7 +4,27 @@ $(() => {
 
   const $res_carousel =  `
   <section id="restaurant-carousel">
-  <article class="restaurant-card">
+  
+        <article class="restaurant-card">
+          <div class="text">
+            <h3>Osmos</h3>
+            <h4>Healthy Food</h4>
+            <small>address</small>
+          </div>
+        </article>
+        <article class="restaurant-card">
+        <div class="text">
+          <h3>Fat Burrito</h3>
+        </div>
+      </article>
+      <article class="restaurant-card">
+      <div class="text">
+        <h3>title</h3>
+        <h4>Healthy Food</h4>
+        <small>address</small>
+      </div>
+    </article>
+    <article class="restaurant-card">
           <div class="text">
             <h3>title</h3>
             <h4>Healthy Food</h4>
