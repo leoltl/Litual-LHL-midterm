7e25141cc24c8e305110e9adb57da878bcc29a6c
Jay Chai <chunsaa@gmail.com>
2019-11-21 21:03:09 +0000

    change phone number for presentation

--- db/seeds/02_restaurants.sql
+++ db/seeds/02_restaurants.sql
@@ -1,5 +1,5 @@
 INSERT INTO restaurants (title, phone, email, password, image)
-VALUES ('freshii', 6472704577, 'freshii@freshii.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://i.imgur.com/F4m0XZY.png');
+VALUES ('freshii', 2892784599, 'freshii@freshii.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://i.imgur.com/F4m0XZY.png');
 
 INSERT INTO restaurants (title, phone, email, password, image)
-VALUES ('mcd', 6472704577, 'mcd@freshii.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://i.imgur.com/F4m0XZY.png');
+VALUES ('mcd', 2892784599, 'mcd@freshii.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://i.imgur.com/F4m0XZY.png');
