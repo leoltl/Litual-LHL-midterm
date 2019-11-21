b2075370e30d9df9565391f47de3b645838f1d91
Leo Lee <tl.leolee@gmail.com>
2019-11-21 21:47:15 +0000

    work so far

---
+++ public/templates/restaurant_carousel.js
@@ -0,0 +1,32 @@
+
+
+$(() => {
+
+  const $res_carousel =  `
+  <section id="restaurant-carousel">
+  <article class="restaurant-card">
+          <div class="text">
+            <h3>title</h3>
+            <h4>Healthy Food</h4>
+            <small>address</small>
+          </div>
+        </article>
+        <article class="restaurant-card">
+        <div class="text">
+          <h3>title</h3>
+          <h4>Healthy Food</h4>
+          <small>address</small>
+        </div>
+      </article>
+      <article class="restaurant-card">
+      <div class="text">
+        <h3>title</h3>
+        <h4>Healthy Food</h4>
+        <small>address</small>
+      </div>
+    </article>
+
+  </section>`
+  window.$res_carousel = $res_carousel
+  $('main').append($res_carousel)
+})
