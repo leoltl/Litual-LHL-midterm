6dd67cf602af0f1309cf81cd92022a6f260e5e6d
Jay Chai <chunsaa@gmail.com>
2019-11-21 21:46:14 +0000

    remove useless comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,11 @@
 $(() => {
-  /* $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done(({users}) => {
-    for(user of users) {
-      console.log(users);
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });; */
-  // delete localStorage.restaurant_id;
-  //localStorage.clear();
-  //localStorage.clear();
+  //call loadCheckoutPage function to load on start for rendering main page
   loadCheckoutPage();
+  //render food menu page on load
   localStorage.res > 0 ?
         $('#main-content article').hide() &&
         $('#restaurant-listing').hide() &&
         findOrders(1).then(res => $order_view.renderOrders(res)) &&
         views_manager.show('order_view') :
         views_manager.show('food_options');
-  console.log("start");
-
-  //views_manager.show('food_options');
 });
